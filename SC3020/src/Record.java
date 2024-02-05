7a7dd8b3768c6dd15adcb5583664f2dd06edd145
Anthony Chew <anthony.chew@hotmail.com>
2024-02-05 19:44:18 +0800

    Update record.java
    Update attribute to the correct datatype and length

--- SC3020/src/Record.java
+++ SC3020/src/Record.java
@@ -1,5 +1,6 @@
 public class Record {
-    public char[] tConst = new char[10];
-    public float averageRating;
-    public int numVotes;
+    //Defining fixed data length for each attribute so record size is fixed
+    public char[] tConst = new char[9]; //18 bytes
+    public byte[] averageRating = new byte[1];//1 byte
+    public byte[] numVotes = new byte[4];//4 bytes
 }
